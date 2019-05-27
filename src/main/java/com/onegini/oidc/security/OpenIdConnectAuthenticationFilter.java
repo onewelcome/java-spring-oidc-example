d25943c708a0e9c65ceeb6b4c4271dcc05249c2d
Jasha Joachimsthal <jasha@onegini.com>
2019-05-27 15:00:14 +0200

    OAUTH-3278 Update dependencies and Spring configuration

--- src/main/java/com/onegini/oidc/security/OpenIdConnectAuthenticationFilter.java
+++ src/main/java/com/onegini/oidc/security/OpenIdConnectAuthenticationFilter.java
@@ -18,7 +18,6 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.stereotype.Component;
 
 import com.nimbusds.jwt.EncryptedJWT;
 import com.nimbusds.jwt.JWT;
@@ -31,10 +30,8 @@ import com.onegini.oidc.model.TokenDetails;
 import com.onegini.oidc.model.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 
-@Component
 @Slf4j
 public class OpenIdConnectAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-
 
   @Resource
   private OAuth2RestOperations oAuth2RestOperations;
