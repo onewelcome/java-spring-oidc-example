1f639db26b7d36adefe2b2934e79a53ffc620860
Jasha Joachimsthal <jasha@onegini.com>
2021-05-20 09:42:49 +0200

    Mock the bean that fetches the OpenID discovery from the OP

--- src/test/java/com/onegini/oidc/ApplicationTest.java
+++ src/test/java/com/onegini/oidc/ApplicationTest.java
@@ -1,65 +1,29 @@
 package com.onegini.oidc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
-
-import java.io.InputStream;
-import java.net.URI;
+import static org.mockito.Mockito.verify;
 
 import javax.annotation.Resource;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.ExpectedCount;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.nimbusds.jose.util.IOUtils;
-import lombok.SneakyThrows;
+import com.onegini.oidc.model.OpenIdDiscovery;
 
 @SpringBootTest(classes = Application.class)
 class ApplicationTest {
 
   @Resource
   private IndexController controller;
-  @Resource
-  private RestTemplate restTemplate;
-
-  private MockRestServiceServer mockServer;
-
-  @BeforeEach
-  void setUp() {
-    mockServer = MockRestServiceServer.createServer(restTemplate);
-
-    mockOpenIdConfiguration();
-  }
+  @MockBean
+  private OpenIdDiscovery openIdDiscovery;
 
   @Test
   void should_start_application_context() {
     assertThat(controller).isNotNull();
-  }
 
-  @SneakyThrows
-  private void mockOpenIdConfiguration() {
-    final ClassPathResource resource = new ClassPathResource("openid-configuration.json");
-
-    try (final InputStream inputStream = resource.getInputStream()) {
-      final String openidConfiguration = IOUtils.readInputStreamToString(inputStream);
-      mockServer.expect(ExpectedCount.once(),
-          requestTo(URI.create("http://localhost:7878/oauth/.well-known/openid-configuration")))
-          .andExpect(method(HttpMethod.GET))
-          .andRespond(withStatus(HttpStatus.OK)
-              .contentType(MediaType.APPLICATION_JSON)
-              .body(openidConfiguration)
-          );
-    }
+    verify(openIdDiscovery).getTokenEndpoint();
   }
 
 }
